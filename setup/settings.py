3c07bef57f1daaf7c9d0b6446a27fa6e30232662
PedroAz <pdegutty@gmail.com>
2023-04-25 12:04:17 -0300

    setting

--- setup/settings.py
+++ setup/settings.py
@@ -8,9 +8,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
