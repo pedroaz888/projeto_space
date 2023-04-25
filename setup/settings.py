cb7a21b867db2be836943d02631305eec0abee48
PedroAz <pdegutty@gmail.com>
2023-04-25 12:36:59 -0300

    staticfiles

--- setup/settings.py
+++ setup/settings.py
@@ -127,7 +127,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static') ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
+STATIC_ROOT = os.path.join('static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
