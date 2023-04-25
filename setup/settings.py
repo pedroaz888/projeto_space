8c56454ed9ed71a1d6bd0f97f7dae250e1766ca6
PedroAz <pdegutty@gmail.com>
2023-04-25 12:17:53 -0300

    database

--- setup/settings.py
+++ setup/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-""" STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static') ]
@@ -131,17 +131,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "/media/" """
+MEDIA_URL = "/media/"
 
 #teste
 
-STATIC_URL = '/static/'
+""" STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
 STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+ """
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
