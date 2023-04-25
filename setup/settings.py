dad545b3fa3b594a9b6e6b1192ea30f514e0484c
PedroAz <pdegutty@gmail.com>
2023-04-25 11:50:00 -0300

    add

--- setup/settings.py
+++ setup/settings.py
@@ -128,11 +128,9 @@ USE_TZ = True
 """ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'setup/static')
-]
+    os.path.join(BASE_DIR, 'setup/static') ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
